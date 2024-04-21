48b73045609414b7fc891ded77d9dee439f07e5d
Aliya Sopokova <alya.sopokova@gmail.com>
2024-04-22 00:24:51 +0400

    Homework1

---
+++ CompareNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Answer {
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        // Введите свое решение ниже
+    if(firstNumber > secondNumber){
+            Console.WriteLine("first number bigger");
+    }
+    else if(firstNumber < secondNumber){
+        Console.WriteLine("second number bigger");
+    }
+    else if(firstNumber == secondNumber){
+        Console.WriteLine("numbers are equel");
+    }
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int firstNumber, secondNumber;
+
+        if (args.Length >= 2) {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            firstNumber = 6;
+            secondNumber = 5;
+        }
+
+        // Не удаляйте строки ниже
+        CompareNumbers(firstNumber, secondNumber);
+    }
+}
